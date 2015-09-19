b717fa9a1035ab767b54297030145232b5e440a4
Mitchell Kember <mk12360@gmail.com>
2015-09-19 18:39:04 -0400

    Style the books index page

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,5 @@
 module BooksHelper
+  def cover_info(book)
+    "#{book.title}<em>#{book.author}</em>".html_safe
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,33 @@ Book.create!(
   author: 'Fyodor Dostoyevsky',
   content: read_file('oliver_twist.html')
 )
+Book.create!(
+  handle: 'la-chute',
+  title: 'La Chute',
+  author: 'Albert Camus',
+  content: read_file('oliver_twist.html')
+)
+Book.create!(
+  handle: '1984',
+  title: '1984',
+  author: 'George Orwell',
+  content: read_file('oliver_twist.html')
+)
+Book.create!(
+  handle: 'how-to-solve-it',
+  title: 'How to Solve It',
+  author: 'George Pólya',
+  content: read_file('oliver_twist.html')
+)
+Book.create!(
+  handle: 'the-hobbit',
+  title: 'The Hobbit',
+  author: 'J.R.R. Tolkien',
+  content: read_file('oliver_twist.html')
+)
+Book.create!(
+  handle: 'the-elements-of-typograhic-style',
+  title: 'The Elements of Typographic Style',
+  author: 'Robert Bringhurst',
+  content: read_file('oliver_twist.html')
+)
