a4c5fa3ef7744eda23ebc5d08e966f469e8d5c0a
Mitchell Kember <mk12360@gmail.com>
2015-09-20 02:20:29 -0400

    Paginate by paragraph

--- lib/paginator.rb
+++ lib/paginator.rb
@@ -10,28 +10,33 @@ class Paginator
   end
 
   def text(page)
-    f = File.open(@path)
-    offset = (page - 1) * @chars_per_page - 1
-    unless offset < 0
+    File.open(@path) do |f|
+      seek_page_start(f, page)
+      read_one_page(f)
+    end
+  end
+
+  private
+
+  def seek_page_start(f, page)
+    offset = (page - 1) * @chars_per_page - 2
+    unless offset <= 0
       f.seek(offset)
-      if f.getc.blank?
-        f.seek(1, IO::SEEK_CUR)
-      else
-        while f.getc.present?
-          f.seek(1, IO::SEEK_CUR)
-        end
+      newline = f.getc == "\n"
+      loop do
+        n = f.getc == "\n"
+        break if newline && n
+        newline = n
       end
     end
+  end
 
-    out = f.read(@chars_per_page)
-    while (c = f.getc).present?
-      out << c
+  def read_one_page(f)
+    out = ""
+    while out.size < @chars_per_page
+      out << f.gets('')
     end
 
     out
-  end
-
-  def close
-    @file.close
   end
 end
