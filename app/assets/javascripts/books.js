8c02b8dc22224c18a74dae279de26c4597f77e75
Michael Min <minmichaelmm@gmail.com>
2015-09-20 00:00:26 -0400

    Add sidebar settings
    Settings to change font, text size and background colour.

---
+++ app/assets/javascripts/books.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$('#FontSize a').click(function() {
+		$('.content').css("font-size", this.dataset.size);
+	});
+
+	$('#FontFamily a').click(function(){
+		$('.content').css("font-family", this.innerHTML);
+	});
+
+	$('#Leading a').click(function(){
+		$('.content').css("line-height", this.dataset.spacing);
+	});
+
+	$('#Background a').click(function() {
+		$('body').css("background-color", $(this).css('background-color'));
+	});
+});
+
